afe020b20be2fbc311edbfa2e93865ba247f5f70
sunny0826 <sunny0826@users.noreply.github.com>
2022-05-24 03:48:53 +0000

    deploy: 8a57bf477fe0741f0205be461462484172a95787

--- README.md
+++
@@ -1,7 +0,0 @@
-# About Me
-
-about me website
-
-## Credit
-
-Based on Cveinnt's awesome [LiveTerm](https://github.com/Cveinnt/LiveTerm).
--- src/utils/bin/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './commands';
-export * from './api_commands';
-export { default as qr } from './qr';
-export { default as contact } from './contact';
