1d6f564126821329a1685d50deaf108f6393e242
guoxudong <guoxudong.dev@gmail.com>
2022-05-24 12:10:00 +0800

    update

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -33,7 +33,7 @@ export const about = async (args: string[]): Promise<string> => {
   return `Hi, I am ${config.name}. 
 Welcome to my website!
 More about me:
-'sumfetch' - short summary.
+'contact' - contact info.
 'resume' - my latest resume.
 'readme' - my github readme.`;
 };
